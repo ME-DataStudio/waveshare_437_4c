47fdbef27de828e0b3e318a3b5ff06d6da723286
ME-DataStudio <75776348+ME-DataStudio@users.noreply.github.com>
2026-06-27 07:56:31 +0200

    Create secrets.h

---
+++ Arduino/secrets.h
@@ -0,0 +1,4 @@
+const char *ssid = "your_ssid";            // The SSID (name) of the Wi-Fi network you want to connect to
+const char *password = "your_password"; // The password of the Wi-Fi network
+// Home Assistant token (get from /profile)
+const char *token = "your long live token homeassistant";
